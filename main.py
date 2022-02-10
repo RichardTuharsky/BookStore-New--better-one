e54e21f9aaac7b164d31af7b99b2f293d81940be
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-02-10 09:16:28 +0100

    Better design a little bit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import*
 from tkinter import ttk
 from datetime import datetime
+from tkinter import font
 import tkinter.messagebox
 from tkcalendar import*
 import datetime
@@ -38,9 +39,27 @@ class Library:
         MainFrame.grid()
         TitleFrame = Frame(MainFrame, bd=10, width=1350, padx=60, relief=RIDGE)
         TitleFrame.pack(side=TOP)
-        self.lblTitle = Label(TitleFrame, width=38, font=(
+        self.lblTitle = Label(TitleFrame, width=43, font=(
             'arial', 40, 'bold'), text="Kniznica")
         self.lblTitle.grid()
+        ButtonFrame = Frame(MainFrame, bd=10, width=1350,
+                            height=50, relief=RIDGE)
+        ButtonFrame.pack(side=BOTTOM)
+        DataFrame = Frame(MainFrame, bd=10, width=1300,
+                          height=400, relief=RIDGE)
+        DataFrame.pack(side=BOTTOM)
+        DataFrameLEFTCover = LabelFrame(
+            DataFrame, bd=10, width=800, height=300, padx=13, pady=2, relief=RIDGE, bg='cadetblue', font=('arial', 12, 'bold'), text="Informacie o uzivateloch")
+        DataFrameLEFTCover.pack(side=LEFT, padx=10)
+        DataFrameLEFT = Frame(DataFrameLEFTCover, bd=10,
+                              width=800, height=300, padx=13, pady=2, relief=RIDGE)
+        DataFrameLEFT.pack(side=TOP)
+        DataFrameLEFTb = LabelFrame(DataFrameLEFTCover, bd=10, width=800, height=100, pady=4,
+                                    padx=10, relief=RIDGE, font=('arial', 12, 'bold'), text="Informacie o uzivateloch")
+        DataFrameLEFTb.pack(side=TOP)
+        DataFrameRIGHT = LabelFrame(DataFrame, bd=10, width=450, height=300, padx=10,
+                                    relief=RIDGE, bg='cadetblue', font=('arial', 12, 'bold'), text="Detaily o knihach")
+        DataFrameRIGHT.pack(side=RIGHT)
 
 
 if __name__ == '__main__':
